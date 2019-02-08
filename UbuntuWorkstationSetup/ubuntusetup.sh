e5ea8adac0465eede3de87808d16e4b43695c1c8
Phil Smallwood <philmore1979@gmail.com>
2019-02-08 11:45:11 -0500

    Added Pulse Secure .deb and adjusted script to install

--- UbuntuWorkstationSetup/ubuntusetup.sh
+++ UbuntuWorkstationSetup/ubuntusetup.sh
@@ -6,7 +6,7 @@ sudo apt update && sudo apt upgrade -y
 
 #Install Software
 ##Software in Default Repos
-sudo apt install virt-manager filezilla vim gdebi keepassx xournal evolution evolution-ews audacity gnumeric vlc chromium-browser git calibre zenmap vim virtualbox vagrant ansible zsh powerline fonts-powerline zsh-theme-powerlevel9k zsh-syntax-highlighting -y
+sudo apt install gimp gnome-tweaks virt-manager filezilla vim gdebi keepassx xournal evolution evolution-ews audacity gnumeric vlc chromium-browser git calibre zenmap vim virtualbox vagrant ansible zsh powerline fonts-powerline zsh-theme-powerlevel9k zsh-syntax-highlighting -y
 ##Install Nextcloud Client
 sudo add-apt-repository ppa:nextcloud-devs/client
 sudo apt update && sudo apt install nextcloud-client -y
@@ -31,6 +31,10 @@ echo "source /usr/share/powerlevel9k/powerlevel9k.zsh-theme" >> ~/.zshrc
 ##Add Syntax Highlighting
 echo "source /usr/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ~/.zshrc
 
-##Still in progress
-#Install Pulse Secure VPN 
+#Install Pulse Secure VPN (deb file in git repo) 
+sudo dpkg -i pulse-9.0R1.x86_64.deb
+sudo apt install libwebkitgtk-1.0-0
+
+
+##Still in progress - CURRENTLY NONE
 
