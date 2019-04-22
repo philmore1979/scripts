53166f46cb475a0d33ceca7ffd33d8a2fe0a3ac0
philmore1979 <philmore1979@colonial.k12.de.us>
2019-04-22 13:31:31 -0400

    Fixed student script section

--- Adobe/adobecsv.sh
+++ Adobe/adobecsv.sh
@@ -5,7 +5,7 @@
 ###
 
 ###Start in correct directory
-cd /home/philmore/scripts/StudentEmailtoEschool
+cd /home/philmore/scripts/Adobe
 
 ###Download Student Cognos files from DOE SFTP
 ##NOTE: At some point before 10/10/2016, DOE changed the config on their SFTP server
@@ -15,9 +15,15 @@ get Cognos/nutritionemails-en.csv
 exit
 EOF
 
-
+##STUDENT CSV FILE SECTION##
 ###Create student csv file with correct headers
 echo "Identity Type,Username,Domain,Email,First Name,Last Name,Country Code,Product Configurations,Admin Roles,Product Configurations Administered,User Groups,User Groups Administered,Products Administered,Developer Access" > adobe_student.csv
-###Read Data from Cognos  File and Put in Right Field
-awk -F',' 'NR>1{print "Federated ID,"$1",colonial.k12.de.us,"$1"@colonial.k12.de.us,"$2","$3",US,Default Spark with Premium Features for K-12 - 2 GB configuration,,,AllStudents,,,}' nutritionemails-en.csv >> adobe_student.csv
+###Read Data from Cognos  File and Put in Right Fields in Temp file
+awk -F',' 'NR>1{print "Federated ID,"$1",colonial.k12.de.us,"$1"@colonial.k12.de.us,"$2","$3",US,Default Spark with Premium Features for K-12 - 2 GB configuration,,,AllStudents,,,"}' nutritionemails-en.csv >> temp_adobe_student.csv
+
+###Remove Duplicates in Student CSV File and put into final student file
+sort -u temp_adobe_student.csv >> adobe_student.csv
+
+##STAFF CSV FILE SECTION##
+##TODO
 
