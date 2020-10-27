f18b450698aee061836f4723f4e46892b58c7ba0
philmore <philmore@polaris.local>
2020-10-27 15:34:40 -0400

    changed forloop logic

--- PikMyKid/pikmykid.sh
+++ PikMyKid/pikmykid.sh
@@ -13,8 +13,8 @@ EOF
 
 
 ###Delete all schools except Wilbur Z-calendar (888) students
-dropschools = [410, 412, 420, 427, 432, 450, 456, 470, 474, 476, 490, 514, 522]
-for i in dropschools
+#dropschools = [410, 412, 420, 427, 432, 450, 456, 470, 474, 476, 490, 514, 522]
+for i in 410 412 420 427 432 450 456 470 474 476 490 514 522
  do `sed -i '/,$i,/d' nutritionemails-en.csv`
 done
 
