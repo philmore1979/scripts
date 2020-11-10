398e1e9e1fe501fa6c3a9d1efdd01922aa41fea8
philmore <philmore@philmore-Virtual-Machine>
2020-11-09 21:02:38 -0500

    adjusted script to include email and logic for filtered or down

--- OffsiteCheck/check.sh
+++ OffsiteCheck/check.sh
@@ -2,11 +2,29 @@
 
 ###OffSite Server Check
 ###Script to scan network servers from off network 
+###DEPENDENCIES
+####Needs NMAP and SSMTP installed
+####SSMTP needs to be configured with an SMTP Host
+
+##Get Timestamp
+Time=$(date)
+
+##Create blank log file
+echo "This script ran at $Time" > log
 
 ###Define Servers
 SERVERS=('34999-inventory.colonial.k12.de.us' 'helpdesk.colonial.k12.de.us' 'jamf.colonial.k12.de.us' '34999-wellness.colonial.k12.de.us')
 
-###Scan them
+###Scan Servers to see if up and log it
 for i in "${SERVERS[@]}"; do 
-	nmap -p 443 $i
+	nmap -p 443 $i >> log
 done
+
+###Look through log and email if not up
+if grep -q down log; then
+	mpack -s "We've got problems!" log philmore1979@gmail.com
+elif grep -q filtered log; then
+	mpack -s "This is weird!" log philmore1979@gmail.com
+else
+	echo "All good" | ssmtp philmore1979@gmail.com
+fi
