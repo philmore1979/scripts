a1cdb182023a2a7e656fe0c273e0616b9bb74b6c
philmore <philmore@polaris.local>
2020-12-03 23:00:39 -0500

    added 3 more reports, fixed logic

--- googleScripts/cbreport.sh
+++ googleScripts/cbreport.sh
@@ -6,36 +6,79 @@
 gam="/Users/philmore/bin/gam/gam"
 ###Set Date for file
 now=`date +%F`
+###Set 30 days ago
+lastmonth=`date -v -30d +%F`
 
-###
+###Input for which report to run
 echo "Which type of report would you like?"
 echo "A) All Devices"
-echo "B) Active Devices"
+echo "B) Active Status Devices"
+echo "C) Active Status Devices Used by Students" 
+echo "D) Active Status Student Devices used in the Last 30 days"
+echo "E) Active Status Student Devices used in the Last 10 days"
+echo "-- "
 read CHOICE
+
+###Keep or Delete temp files
+echo "Would you like to keep the temp files? (N/y)"
+echo "-- " 
+read DELTEMP
+
+###Get the newest data from Google
+$gam print cros fields status,lastsync,serialnumber,recentUsers listlimit 1 > cbdatacurrent.csv
 
 ###All Devices
 if [[ $CHOICE == "A" ]] || [[ $CHOICE == "a" ]]; then
-	$gam print cros fields status,lastsync,serialnumber,recentUsers listlimit 1 > cbdatacurrent.csv
 	echo "SerialNumber,User,Date,DeviceID,Status" > CBReport-$now.csv
 	awk -F',' 'NR>1{print $2","$6","$4","$1","$3}' cbdatacurrent.csv >> CBReport-$now.csv 
-	rm cbdatacurrent.csv
-###Only Active Devices
+
+###Active Status Devices
 elif [[ $CHOICE == "B" ]] || [[ $CHOICE == "b" ]]; then
-	$gam print cros fields status,lastsync,serialnumber,recentUsers listlimit 1 > cbdatacurrent.csv
 	grep -F ',ACTIVE,' cbdatacurrent.csv > cbdatacurrent_active.csv
 	echo "SerialNumber,User,Date,DeviceID" > CBReport_Active-$now.csv
 	awk -F',' '{print $2","$6","$4","$1}' cbdatacurrent_active.csv >> CBReport_Active-$now.csv
-	rm cbdatacurrent.csv
-###Only Active Student Devices
+
+###Active Status Devices Used by Students
 elif [[ $CHOICE == "C" ]] || [[ $CHOICE == "c" ]]; then
-	$gam print cros fields status,lastsync,serialnumber,recentUsers listlimit 1 > cbdatacurrent.csv
         grep -F ',ACTIVE,' cbdatacurrent.csv > cbdatacurrent_active.csv	
 	cat cbdatacurrent_active.csv | grep '[0-9][0-9][0-9][0-9][0-9][0-9]@colonial.k12.de.us' > cbdatacurrent_activestudent.csv
 	echo "SerialNumber,User,Date,DeviceID" > CBReport_ActiveStudents-$now.csv
         awk -F',' '{print $2","$6","$4","$1}' cbdatacurrent_activestudent.csv >> CBReport_ActiveStudents-$now.csv
-        rm cbdatacurrent*.csv
+
+###Active Status Devices, Used by Students, Used in last 30 days
+elif [[ $CHOICE == "D" ]] || [[ $CHOICE == "d" ]]; then
+	grep -F ',ACTIVE,' cbdatacurrent.csv > cbdatacurrent_active.csv
+        cat cbdatacurrent_active.csv | grep '[0-9][0-9][0-9][0-9][0-9][0-9]@colonial.k12.de.us' > cbdatacurrent_activestudent.csv
+        for i in `seq 0 30`; do 
+		day=`date -v -"$i"d  +"%F"`
+		cat cbdatacurrent_activestudent.csv | grep $day >> cbdatacurrent_activestudent30.csv
+ 	done
+	echo "SerialNumber,User,Date,DeviceID" > CBReport_ActiveStudentsLast30-$now.csv
+        awk -F',' '{print $2","$6","$4","$1}' cbdatacurrent_activestudent30.csv >> CBReport_ActiveStudentsLast30-$now.csv	
+
+###Active Status Devices, Used by Students, Used in last 10 days
+elif [[ $CHOICE == "E" ]] || [[ $CHOICE == "e" ]]; then
+        grep -F ',ACTIVE,' cbdatacurrent.csv > cbdatacurrent_active.csv
+        cat cbdatacurrent_active.csv | grep '[0-9][0-9][0-9][0-9][0-9][0-9]@colonial.k12.de.us' > cbdatacurrent_activestudent.csv
+        for i in `seq 0 10`; do
+                day=`date -v -"$i"d  +"%F"`
+                cat cbdatacurrent_activestudent.csv | grep $day >> cbdatacurrent_activestudent10.csv
+        done
+        echo "SerialNumber,User,Date,DeviceID" > CBReport_ActiveStudentsLast10-$now.csv
+        awk -F',' '{print $2","$6","$4","$1}' cbdatacurrent_activestudent10.csv >> CBReport_ActiveStudentsLast10-$now.csv
+
+###Active Status Devices, Used by Students, Used in last 10 days
+elif [[ $CHOICE == "F" ]] || [[ $CHOICE == "f" ]]; then
+	echo "Not Ready Yet"
 ###Non-option
 else
 	echo "You lose, sir! Good Day!!"
 fi
+
+if [[ $DELTEMP == "Y" ]] || [[ $DELTEMP == "y" ]]; then
+	echo "Files Retained"
+else
+	rm cbdatacurrent*.csv
+fi
+
 echo "Thank you"
